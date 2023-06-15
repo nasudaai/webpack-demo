7f38c395d2526277b78c2525bf0e76419e1dcb96
nasuda <nasuda@example.com>
2023-06-15 22:54:20 +0900

    Removed source maps when building

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 
+const isDev = process.env.NODE_ENV === "development";
+
 module.exports = {
+    mode: isDev ? "development" : "production",
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    devtool: 'inline-source-map',
+    devtool: isDev ? 'inline-source-map' : undefined,
     devServer: {
         static: './dist'
     },
